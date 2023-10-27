50e6217e0271ed9934e253c303a37daebe59bba6
vikas <vikas@smartworking-solutions.com>
2023-10-27 20:47:49 +0530

    Add loader while uploading video and also do reset video player after successful uploading

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Button } from '@mui/material';
+import { Button, Alert, CircularProgress } from '@mui/material';
 import axios from 'axios';
-import "./App.css";
+import "./App.scss";
 
-const App: React.FC = () => {
+const App = (): React.ReactElement => {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const [uploading, setUploading] = useState(false);
+  const [messageState, setMessageState] = useState<string | undefined>();
   const previewVideoRef = useRef<HTMLVideoElement>(null); // Added preview video reference
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
   const [recordedChunks, setRecordedChunks] = useState<Blob[]>([]);
@@ -14,10 +16,19 @@ const App: React.FC = () => {
       const recordedBlob = new Blob(recordedChunks, { type: 'video/webm' });
       previewVideoRef.current.src = URL.createObjectURL(recordedBlob);
     }
-  },[recordedChunks])
+  },[recordedChunks]);
+
+  useEffect(() => {
+    if(messageState){
+      setTimeout(() => {
+        setMessageState(undefined)
+      },5000)
+    }
+  }, [messageState]);
+
 
   const handleStartRecording = () => {
-    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
       videoRef.current.srcObject = stream;
       mediaRecorderRef.current = new MediaRecorder(stream);
       mediaRecorderRef.current.ondataavailable = handleDataAvailable;
@@ -46,20 +57,22 @@ const App: React.FC = () => {
 
   const handleUpload = async () => {
     if(recordedChunks.length) {
+      setUploading(true);
       const blob = new Blob(recordedChunks, {type: 'video/webm'});
       const formData = new FormData();
       formData.append('video', blob);
-
       try {
         await axios.post('http://localhost:3001/upload', formData, {
           headers: {
             'Content-Type': 'multipart/form-data'
           }
         });
-        alert('Video uploaded successfully.');
-        setRecordedChunks([])
-      } catch (error) {
-        alert('Failed to upload video.');
+        setMessageState("success");
+        setRecordedChunks([]);
+      } catch {
+        setMessageState("error");
+      } finally {
+        setUploading(false);
       }
     }
   };
@@ -75,9 +88,12 @@ const App: React.FC = () => {
         <Button variant="outlined" disabled={!videoRef?.current?.srcObject} color="secondary" onClick={handleStopRecording}>
           Stop Recording
         </Button>
-        <Button disabled={!recordedChunks.length} variant="contained" onClick={handleUpload}>
+        <Button disabled={!recordedChunks.length || uploading} variant="contained" onClick={handleUpload}>
+          {uploading && <CircularProgress size={20} style={{ marginRight: 5 }} />}
           Upload
         </Button>
+        {messageState === "error" && <Alert severity="error">Error video uploading to Minio!</Alert>}
+        {messageState === "success" && <Alert severity="success">Video uploaded successfully to Minio!</Alert>}
       </div>
   );
 };
